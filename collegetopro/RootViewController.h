20cce0a176a86840069ee45bad0e1b1d004814f2
James <james@breezy.com>
2012-01-19 21:44:16 -0800

    Initial Commit

---
+++ collegetopro/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  collegetopro
+//
+//  Created by James Graham on 1/19/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
---
+++ collegetopro/collegetoproAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  collegetoproAppDelegate.h
+//  collegetopro
+//
+//  Created by James Graham on 1/19/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface collegetoproAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
